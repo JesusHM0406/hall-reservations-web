ab5e297326f45379191d39f987e21aea2e56cc3a
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-10 11:58:44 -0600

    feat: add open/close state for the Sidebar with animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import Sidebar from './components/layout/Sidebar';
+import { SidebarOpen } from 'lucide-react';
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState<boolean>(false)
+
   return (
-    <>
-      <Sidebar />
-    </>
+    <div className='flex min-h-dvh w-full overflow-x-hidden'>
+      <Sidebar isSidebarOpen={isSidebarOpen} closeMethod={() => setIsSidebarOpen(false)} />
+      <div className='min-w-full'>
+        <SidebarOpen onClick={() => setIsSidebarOpen(true)} />
+        <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque dignissimos consectetur itaque tempore. Iste, ipsam dolorum odio labore ipsum nulla fugit impedit enim magnam maxime et quod aliquam dicta aspernatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores atque consequatur quisquam a similique magnam placeat quasi fuga reprehenderit eaque suscipit ex vitae, quidem odit eius ratione deleniti iure. Exercitationem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda, cum ad. Error quam aspernatur doloribus cum, id illum ullam quod, numquam officia corporis nulla vitae? Alias fuga repellat sunt ratione? Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, laboriosam sed autem repellat omnis delectus ipsam aspernatur amet voluptates voluptas ratione harum numquam sequi excepturi eius iusto quis nostrum velit! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat facilis obcaecati molestiae ut, dicta deleniti totam consequuntur distinctio maiores quasi. Veritatis nesciunt nulla aspernatur, omnis nisi eos quos voluptate esse! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod quibusdam odio ipsum. Ea, officia. Reprehenderit, ut. Porro itaque sequi eius doloremque pariatur deleniti, possimus incidunt, earum veritatis laudantium, vitae aliquid? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, deleniti saepe id dicta tempore sit eveniet porro sunt. Explicabo dolor molestiae nostrum. Nobis, unde. Provident illum ea quis deleniti ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ad doloremque voluptatem. Iste, atque nostrum totam iure a deserunt incidunt corrupti. Reprehenderit saepe dolor similique earum, vitae maiores ad aperiam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio sed nulla distinctio dicta vel, ipsa sapiente tenetur suscipit magni? Minima iusto ad eveniet accusamus expedita quasi soluta! At, totam quae.</span>
+      </div>
+    </div>
   )
 };
 
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+import { useState, type FC } from 'react';
 import { SIDEBAR_ITEMS } from '../../config/sidebar-config';
 import Button from '../common/Button';
 import SidebarItem from '../ui/SidebarItem';
 
-const Sidebar = () => {
+interface SidebarProps {
+  isSidebarOpen: boolean;
+  closeMethod: () => void;
+}
+
+const Sidebar: FC<SidebarProps> = ({ isSidebarOpen, closeMethod }) => {
   const [expandedSection, setExpandedSection] = useState<string | null>(null);
   const [activeSection, setActiveSection] = useState<string |null>(null);
   const [activeOpt, setActiveOpt] = useState<string | null>(null);
@@ -17,22 +22,25 @@ const Sidebar = () => {
     setExpandedSection(null);
     setActiveOpt(null);
     setActiveSection(sectionLabel);
+    closeMethod();
   };
 
   const handleOptClick = (optLabel: string, sectionLabel: string) => {
     setActiveOpt(optLabel + sectionLabel);
     setActiveSection(sectionLabel);
+    closeMethod();
   };
 
   const handleButtonClick = () => {
     setActiveOpt(null);
     setActiveSection(null);
     setExpandedSection(null);
+    closeMethod();
   };
 
   return (
     <>
-      <aside className='bg-neutral-900 w-70 h-dvh flex flex-col px-4'>
+      <aside className={`shrink-0 w-70 px-4 flex flex-col transition-[margin] overflow-hidden ${isSidebarOpen ? 'ml-0' : '-ml-70'} md:ml-0 z-10 bg-neutral-900`}>
         <div className='grow mt-7'>
           {SIDEBAR_ITEMS.map(item => {
             if (item.type === 'expandable') {
@@ -65,6 +73,7 @@ const Sidebar = () => {
           <Button label='Log In' iconName='log-in' primary={false} clickMethod={handleButtonClick} />
         </div>
       </aside>
+      <div className={`bg-black/50 w-dvw h-dvh absolute z-0 ${!isSidebarOpen && 'hidden'} md:hidden`} onClick={closeMethod}></div>
     </>
   )
 };

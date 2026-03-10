661cedb0b318902c7a217821b1048b79ecbc18b1
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-10 16:38:07 -0600

    refactor: update hall submenu options

--- src/config/sidebar-config.ts
+++ src/config/sidebar-config.ts
@@ -16,9 +16,8 @@ type ExpandableItemConfig = {
 type ItemConfig = SimpleItemConfig | ExpandableItemConfig
 
 export const SIDEBAR_ITEMS: ItemConfig[] = [
-  { type: 'expandable', sectionLabel: 'Search Halls', iconName: 'book-search', optLabels: ['By ID', 'Advanced Search'] },
+  { type: 'expandable', sectionLabel: 'Halls', iconName: 'house', optLabels: ['Search Halls', 'All Halls', 'New Hall', 'Update Hall'] },
   { type: 'expandable', sectionLabel: 'Reservations', iconName: 'calendar', optLabels: ['New Reservation', 'My Reservations', 'All Reservations', 'By ID'] },
   { type: 'expandable', sectionLabel: 'Users', iconName: 'users', optLabels: ['By ID', 'All Users', 'Update User', 'Delete User'] },
-  { type: 'expandable', sectionLabel: 'Manage Halls', iconName: 'wrench', optLabels: ['New Hall', 'Update Hall'] },
   { type: 'simple', sectionLabel: 'My Account', iconName: 'user' }
 ];

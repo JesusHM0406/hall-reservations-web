821d9cf3fad3e0dc22860480aa3ee5b960e59cb6
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-10 10:55:03 -0600

    feat: add smoth transitions for the color to SidebarItem component, and for the rotation fo the arrow

--- src/components/ui/SidebarItem.tsx
+++ src/components/ui/SidebarItem.tsx
@@ -31,10 +31,10 @@ const SidebarItem: FC<SidebarItemProps> = (props: SidebarItemProps) => {
 
     return (
       <div className='flex flex-col my-3'>
-        <div className={`p-0.5 cursor-pointer flex items-center gap-2 ${isActive ? 'text-white' : 'text-slate-400'}`} onClick={sectionMethod}>
+        <div className={`p-0.5 cursor-pointer flex items-center gap-2 transition-color duration-100 ${isActive ? 'text-white' : 'text-slate-400'}`} onClick={sectionMethod}>
           <DynamicIcon name={iconName} size={18} />
           <span className='grow font-bold'>{sectionLabel}</span>
-          <ChevronRight size={18} className={`${isExpanded ? 'rotate-90' : ''}`} />
+          <ChevronRight size={18} className={`transition-transform duration-200 ${isExpanded ? 'rotate-90' : ''}`} />
         </div>
         <div className={`mt-0.5 grid transform-all duration-200 grid-rows-[0fr] ${isExpanded && 'grid-rows-[1fr]'}`}>
           <div className='overflow-hidden flex flex-col gap-1'>

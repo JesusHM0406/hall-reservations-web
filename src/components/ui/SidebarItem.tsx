6498a44af4532464730e93cf07560331b669074e
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-10 10:45:00 -0600

    fix: use grid-rows-* instead of grid-cols-* and rotate the arrow icon when the section is expanded

--- src/components/ui/SidebarItem.tsx
+++ src/components/ui/SidebarItem.tsx
@@ -34,9 +34,9 @@ const SidebarItem: FC<SidebarItemProps> = (props: SidebarItemProps) => {
         <div className={`p-0.5 cursor-pointer flex items-center gap-2 ${isActive ? 'text-white' : 'text-slate-400'}`} onClick={sectionMethod}>
           <DynamicIcon name={iconName} size={18} />
           <span className='grow font-bold'>{sectionLabel}</span>
-          <ChevronRight size={18} />
+          <ChevronRight size={18} className={`${isExpanded ? 'rotate-90' : ''}`} />
         </div>
-        <div className={`mt-0.5 grid transform-all duration-250 grid-cols-[0fr] ${isExpanded && 'grid-cols-[1fr]'}`}>
+        <div className={`mt-0.5 grid transform-all duration-200 grid-rows-[0fr] ${isExpanded && 'grid-rows-[1fr]'}`}>
           <div className='overflow-hidden flex flex-col gap-1'>
             {optLabels.map((optLabel, index) => {
               return (
@@ -57,7 +57,7 @@ const SidebarItem: FC<SidebarItemProps> = (props: SidebarItemProps) => {
 
   // If is not 'expandable' then it is necessarily 'simple'
   return (
-    <div className={`cursor-pointer p-0.5 flex items-center gap-2 ${isActive ? 'text-white' : 'text-slate-300'}`} onClick={sectionMethod}>
+    <div className={`cursor-pointer p-0.5 flex items-center gap-2 ${isActive ? 'text-white' : 'text-slate-400'}`} onClick={sectionMethod}>
       <DynamicIcon name={iconName} size={18} />
       <span className='grow font-bold'>{sectionLabel}</span>
     </div>

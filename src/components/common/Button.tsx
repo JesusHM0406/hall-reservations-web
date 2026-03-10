7a963190534ee345ebec469732c78d6c36e75116
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-10 10:51:34 -0600

    feat: implement state logic to Sidebar component and add the click method for the button

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -5,11 +5,12 @@ interface ButtonProps {
   label: string;
   iconName?: IconName;
   primary: boolean;
+  clickMethod: () => void;
 }
 
-const Button: FC<ButtonProps> = ({ label, iconName, primary }) => {
+const Button: FC<ButtonProps> = ({ label, iconName, primary, clickMethod }) => {
   return (
-    <button className={`cursor-pointer text-white w-fit font-semibold rounded py-2 px-4 border-2 transition-all ${primary ? 'bg-indigo-400 border-transparent' : 'bg-transparent border-indigo-400'} flex gap-2 hover:shadow-lg hover:shadow-indigo-400/45`}>
+    <button className={`cursor-pointer text-white w-fit font-semibold rounded py-2 px-4 border-2 transition-all ${primary ? 'bg-indigo-400 border-transparent' : 'bg-transparent border-indigo-400'} flex gap-2 hover:shadow-lg hover:shadow-indigo-400/45`} onClick={clickMethod}>
       {iconName && <DynamicIcon name={iconName} size={18} />}
       {label}
     </button>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,8 +1,35 @@
+import { useState } from 'react';
 import { SIDEBAR_ITEMS } from '../../config/sidebar-config';
 import Button from '../common/Button';
 import SidebarItem from '../ui/SidebarItem';
 
 const Sidebar = () => {
+  const [expandedSection, setExpandedSection] = useState<string | null>(null);
+  const [activeSection, setActiveSection] = useState<string |null>(null);
+  const [activeOpt, setActiveOpt] = useState<string | null>(null);
+
+  const handleSectionClick = (sectionLabel: string, hasOptions: boolean) => {
+    if (hasOptions) {
+      setExpandedSection(expandedSection === sectionLabel ? null : sectionLabel);
+      return;
+    }
+
+    setExpandedSection(null);
+    setActiveOpt(null);
+    setActiveSection(sectionLabel);
+  };
+
+  const handleOptClick = (optLabel: string, sectionLabel: string) => {
+    setActiveOpt(optLabel + sectionLabel);
+    setActiveSection(sectionLabel);
+  };
+
+  const handleButtonClick = () => {
+    setActiveOpt(null);
+    setActiveSection(null);
+    setExpandedSection(null);
+  };
+
   return (
     <>
       <aside className='bg-neutral-900 w-70 h-dvh flex flex-col px-4'>
@@ -12,11 +39,11 @@ const Sidebar = () => {
               return (
                 <SidebarItem
                   {...item}
-                  isActive={false}
-                  sectionMethod={() => {}}
-                  isExpanded={true}
-                  optMethod={() => {}}
-                  activeOpt={null}
+                  isActive={activeSection === item.sectionLabel}
+                  sectionMethod={() => handleSectionClick(item.sectionLabel, true)}
+                  isExpanded={expandedSection === item.sectionLabel}
+                  optMethod={(optLabel: string) => handleOptClick(optLabel, item.sectionLabel)}
+                  activeOpt={activeOpt}
                   key={item.sectionLabel}
                 />
               )
@@ -25,8 +52,8 @@ const Sidebar = () => {
             return (
               <SidebarItem
                 {...item}
-                isActive={true}
-                sectionMethod={() => {}}
+                isActive={activeSection === item.sectionLabel}
+                sectionMethod={() => handleSectionClick(item.sectionLabel, false)}
                 key={item.sectionLabel}
               />
             )
@@ -34,8 +61,8 @@ const Sidebar = () => {
           })}
         </div>
         <div className='flex flex-col gap-4 items-center mb-10'>
-          <Button label='Register' iconName='plus' primary={true} />
-          <Button label='Log In' iconName='log-in' primary={false} />
+          <Button label='Register' iconName='plus' primary={true} clickMethod={handleButtonClick} />
+          <Button label='Log In' iconName='log-in' primary={false} clickMethod={handleButtonClick} />
         </div>
       </aside>
     </>

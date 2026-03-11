8036c27852749cb39566bd1b84f94fb668a3de6a
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-10 18:35:25 -0600

    fix: use type button in the Button component to prevent unexpected form submissions

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 
 const Button: FC<ButtonProps> = ({ label, iconName, primary, clickMethod }) => {
   return (
-    <button className={`cursor-pointer text-white w-fit font-semibold rounded py-2 px-4 border-2 transition-all ${primary ? 'bg-indigo-400 border-transparent' : 'bg-transparent border-indigo-400'} flex gap-2 hover:shadow-lg hover:shadow-indigo-400/45`} onClick={clickMethod}>
+    <button type='button' className={`cursor-pointer text-white w-fit font-semibold rounded py-2 px-4 border-2 transition-all ${primary ? 'bg-indigo-400 border-transparent' : 'bg-transparent border-indigo-400'} flex gap-2 hover:shadow-lg hover:shadow-indigo-400/45`} onClick={clickMethod}>
       {iconName && <DynamicIcon name={iconName} size={18} />}
       {label}
     </button>

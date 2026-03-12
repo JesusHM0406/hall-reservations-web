c8cff277737010eb6e50db91806350304abec663
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-11 20:12:10 -0600

    chore: create index file for the pages folder

---
+++ src/pages/index.ts
@@ -0,0 +1,29 @@
+import SearchHallsPage from "../pages/halls/SearchHallsPage";
+import AllHallsPage from "../pages/halls/AllHallsPage";
+import NewHallPage from "../pages/halls/NewHallPage";
+import UpdateHallPage from "../pages/halls/UpdateHallPage";
+import NewReservationPage from "../pages/reservations/NewReservationPage";
+import AllReservationsPage from "../pages/reservations/AllReservationsPage";
+import MyReservationsPage from "../pages/reservations/MyReservationsPage";
+import ReservationByIDPage from "../pages/reservations/ReservationByIDPage";
+import AllUsersPage from "../pages/users/AllUsersPage";
+import UpdateUserPage from "../pages/users/UpdateUserPage";
+import DeleteUserPage from "../pages/users/DeleteUserPage";
+import UserByIDPage from "../pages/users/UserByIDPage";
+import MyAccountPage from "../pages/my-account/MyAccountPage";
+
+export {
+  SearchHallsPage,
+  AllHallsPage,
+  NewHallPage,
+  UpdateHallPage,
+  NewReservationPage,
+  AllReservationsPage,
+  MyReservationsPage,
+  ReservationByIDPage,
+  AllUsersPage,
+  UpdateUserPage,
+  DeleteUserPage,
+  UserByIDPage,
+  MyAccountPage
+};

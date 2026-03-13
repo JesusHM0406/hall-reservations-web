c6212192b70f030e83ce8c178d318dfbc3437ea0
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-13 14:23:14 -0600

    refactor: include button tag attributes and add default values in the Button component

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,17 +1,17 @@
 import { Slot } from '@radix-ui/react-slot';
-import type { ReactNode } from 'react';
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
 
-interface ButtonProps {
-  primary: boolean;
-  asChild: boolean;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  primary?: boolean;
+  asChild?: boolean;
   children: ReactNode;
 }
 
-const Button = ({ primary, asChild, children }: ButtonProps) => {
+const Button = ({ primary = false, asChild = false, children, ...props }: ButtonProps) => {
   const Comp = asChild ? Slot : 'button';
 
   return (
-    <Comp className={`cursor-pointer text-white flex justify-center items-center gap-2.5 font-semibold rounded-xl py-3 px-4 border-2 transition-shadow ${primary ? 'bg-brand border-transparent' : 'bg-transparent border-brand'} shadow-lg shadow-transparent hover:shadow-brand/45`} >
+    <Comp className={`cursor-pointer text-white flex justify-center items-center gap-2.5 font-semibold rounded-xl py-3 px-4 border-2 transition-shadow ${primary ? 'bg-brand border-transparent' : 'bg-transparent border-brand'} shadow-lg shadow-transparent hover:shadow-brand/45`} {...props} >
       {children}
     </Comp>
   )
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -62,21 +62,15 @@ const Sidebar = ({ isSidebarOpen, closeMethod }: SidebarProps) => {
           })}
         </div>
         <div className='flex flex-col gap-4 items-center'>
-          <Button primary asChild>
-            <Link
-              to={`/${PATHS.auth.root}/${PATHS.auth.register}`}
-              onClick={handleAuthButtonClick}
-            >
+          <Button primary asChild onClick={handleAuthButtonClick}>
+            <Link to={`/${PATHS.auth.root}/${PATHS.auth.register}`}>
               <UserPlus size={ICON_SIZE.MD} />
               <span>Register</span>
             </Link>
           </Button>
           
-          <Button primary={false} asChild>
-            <Link
-              to={`/${PATHS.auth.root}/${PATHS.auth.login}`}
-              onClick={handleAuthButtonClick}
-            >
+          <Button asChild onClick={handleAuthButtonClick}>
+            <Link to={`/${PATHS.auth.root}/${PATHS.auth.login}`}>
               <LogIn size={ICON_SIZE.MD} />
               <span>Log In</span>
             </Link>

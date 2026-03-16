b57f0ca21a8b44bbecc7e0630aed782252ad4225
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-16 16:22:35 -0600

    fix: hide the sidebar close button in md breakpoint

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -57,7 +57,7 @@ const Sidebar = ({ isSidebarOpen, closeMethod, className, ...props }: SidebarPro
           <button 
             ref={closeBtnRef} 
             aria-label='Close main navigation sidebar'
-            className='cursor-pointer p-3 w-fit rounded-xl border-2 border-brand text-inactive mb-6 self-end transition-colors duration-150 hover:bg-brand hover:text-white' 
+            className='cursor-pointer p-3 w-fit rounded-xl border-2 border-brand text-inactive mb-6 self-end transition-colors duration-150 hover:bg-brand hover:text-white md:hidden' 
             onClick={closeMethod}
           >
             <X size={ICON_SIZE.SM} aria-hidden='true' />

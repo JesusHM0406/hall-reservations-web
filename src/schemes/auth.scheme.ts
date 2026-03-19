3b05fece08715cbc0ef9abdb20b1a8f4d4a67961
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-18 20:37:33 -0600

    chore(schemes): create validation scheme for the login form

--- src/schemes/auth.scheme.ts
+++ src/schemes/auth.scheme.ts
@@ -23,5 +23,22 @@ const registerScheme = z
 
 type RegisterFormData = z.infer<typeof registerScheme>;
 
-export { registerScheme };
-export type { RegisterFormData };
+const loginScheme = z
+  .object({
+    username: z
+      .string()
+      .trim()
+      .min(3, 'The name must contain at least 3 characters')
+      .max(30, 'The name cannot contain more than 30 characters'),
+    password: z
+      .string()
+      .min(8, 'The password must contain at least 8 characters')
+      .max(72, 'The password cannot contain more than 72 characters')
+      .regex(/[A-Z]/, 'Must contain at least one capital letter')
+      .regex(/[0-9]/, 'Must contain at least one number')
+  });
+
+type LoginFormData = z.infer<typeof loginScheme>;
+
+export { registerScheme, loginScheme };
+export type { RegisterFormData, LoginFormData };

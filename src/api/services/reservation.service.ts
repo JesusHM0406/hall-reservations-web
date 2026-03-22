bff6d69fd8c2d42580b3c1f372c02d75a7ba5ffd
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-22 07:38:49 -0600

    feat: add all reservation service functions

---
+++ src/api/services/reservation.service.ts
@@ -0,0 +1,29 @@
+import { apiRequest } from '../axios';
+import { API_ENDPOINTS } from '../endpoints';
+import type { ReservationCreate, ReservationPaginationParams } from '../schemas/reservation.schemas';
+
+export const reservationService = {
+  async create(payload: ReservationCreate) {
+    await apiRequest(API_ENDPOINTS.RESERVATIONS.ADD(payload));
+  },
+
+  async all(filters: ReservationPaginationParams) {
+    return await apiRequest(API_ENDPOINTS.RESERVATIONS.ALL(filters));
+  },
+
+  async allCurrentUser(filters: ReservationPaginationParams) {
+    return await apiRequest(API_ENDPOINTS.RESERVATIONS.ALL_CURRENT_USER(filters));
+  },
+
+  async byId(id: number | string) {
+    return await apiRequest(API_ENDPOINTS.RESERVATIONS.BY_ID(id));
+  },
+
+  async cancel(id: number | string) {
+    await apiRequest(API_ENDPOINTS.RESERVATIONS.CANCEL(id));
+  },
+
+  async finish(id: number | string) {
+    await apiRequest(API_ENDPOINTS.RESERVATIONS.FINISH(id));
+  }
+};

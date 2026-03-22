11309b436161bed97facb95cf469e933d5e29c63
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-22 11:36:42 -0600

    chore: remove unnecessary type property for the sidebar items config'

--- src/config/sidebar-config.ts
+++ src/config/sidebar-config.ts
@@ -2,7 +2,6 @@ import { type IconName } from 'lucide-react/dynamic';
 import { PATHS } from '../paths';
 
 interface ItemConfig {
-  type: 'simple';
   sectionLabel: string;
   iconName: IconName;
   rootPath: string;
@@ -10,25 +9,21 @@ interface ItemConfig {
 
 const SIDEBAR_ITEMS: ItemConfig[] = [
   {
-    type: 'simple',
     sectionLabel: 'Halls',
     iconName: 'house',
     rootPath: PATHS.halls
   },
   {
-    type: 'simple',
     sectionLabel: 'Reservations',
     iconName: 'calendar',
     rootPath: PATHS.reservations
   },
   {
-    type: 'simple',
     sectionLabel: 'Users',
     iconName: 'users',
     rootPath: PATHS.users
   },
   {
-    type: 'simple',
     sectionLabel: 'My Account',
     iconName: 'user',
     rootPath: PATHS.myAccount

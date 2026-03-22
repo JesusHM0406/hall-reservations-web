50528a73f80a67b3896218ff3d1c3d664aa991b0
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-22 07:37:56 -0600

    feat: add all user service functions

--- src/api/services/user.service.ts
+++ src/api/services/user.service.ts
@@ -1,8 +1,37 @@
 import { apiRequest } from '../axios';
 import { API_ENDPOINTS } from '../endpoints';
+import { type UserAdminUpdate, type UserPaginationParams, type UserUpdate } from '../schemas/user.schemas';
 
 export const userService = {
   async getCurrent() {
     return await apiRequest(API_ENDPOINTS.USERS.READ_CURRENT());
+  },
+
+  async getAll(filters: UserPaginationParams) {
+    return await apiRequest(API_ENDPOINTS.USERS.ALL(filters));
+  },
+
+  async deleteCurrent() {
+    await apiRequest(API_ENDPOINTS.USERS.DELETE_CURRENT());
+  },
+
+  async updateCurrent(payload: UserUpdate) {
+    await apiRequest(API_ENDPOINTS.USERS.UPDATE_CURRENT(payload));
+  },
+
+  async byId(id: number | string) {
+    return await apiRequest(API_ENDPOINTS.USERS.BY_ID(id));
+  },
+
+  async update(id: string | number, payload: UserAdminUpdate) {
+    await apiRequest(API_ENDPOINTS.USERS.UPDATE(id, payload));
+  },
+
+  async delete(id: string | number) {
+    await apiRequest(API_ENDPOINTS.USERS.DELETE(id));
+  },
+
+  async restore(id: string | number, newName: UserUpdate) {
+    await apiRequest(API_ENDPOINTS.USERS.RESTORE(id, newName));
   }
 };

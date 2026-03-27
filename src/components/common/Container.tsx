4e22a152b9e176f5c045435ab5d41e47ab5059e1
Jesus HM <jesus.h.mora0406@gmail.com>
2026-03-27 09:37:42 -0600

    style: use the same background colors in the container component as in the input

--- src/components/common/Container.tsx
+++ src/components/common/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container = ({ children }: ContainerProps) => {
   return (
-    <div className='p-7 rounded-4xl bg-slate-50 border border-transparent shadow-lg shadow-dark/10 dark:bg-slate-950 dark:border-slate-gray'>
+    <div className='p-6 border border-inactive/25 max-w-md rounded-2xl shadow-xl shadow-dark-gray/5 dark:bg-dark-gray dark:border-slate-gray'>
       {children}
     </div>
   )

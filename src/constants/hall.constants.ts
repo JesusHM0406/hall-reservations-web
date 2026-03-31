3bf7ce22c1920b3f1be7b73703854127b948f982
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-03-31 16:52:23 -0600

    chore: create hall.constants file

---
+++ src/constants/hall.constants.ts
@@ -0,0 +1,19 @@
+import type { FilterItem } from '@/components/ui/FilterSelect';
+import { hallAvailabilityFilterEnum, type HallAvailabilityFilter } from '@/api/schemas/hall.schemas';
+
+const HALL_AVAILABILITY_MAP: Record<HallAvailabilityFilter, FilterItem<HallAvailabilityFilter>> = {
+  [hallAvailabilityFilterEnum.enum.all]: {
+    value: 'all',
+    label: 'All'
+  },
+  [hallAvailabilityFilterEnum.enum.available]: {
+    value: 'available',
+    label: 'Available'
+  },
+  [hallAvailabilityFilterEnum.enum.unavailable]: {
+    value: 'unavailable',
+    label: 'Unavailable'
+  }
+};
+
+export const HALL_AVAILABILITY_FILTER_ITEMS = Object.values(HALL_AVAILABILITY_MAP);

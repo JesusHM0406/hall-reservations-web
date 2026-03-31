d6b6bd5aaa62b51d0c2c6b28cd19488fc390c5af
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-03-31 16:56:53 -0600

    chore: delete HallPage.tsx file since it is no longer in use

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
 import { MyAccountPage } from './MyAccountPage';
 import { RegisterPage } from './auth/RegisterPage';
 import { LoginPage } from './auth/LoginPage';
-import { HallsPage } from './halls/HallsPage';
 import { ReservationsPage } from './ReservationsPage';
 import { UsersPage } from './UsersPage';
 import { HallDetailPage } from './halls/HallDetailPage';
@@ -9,7 +8,6 @@ import { HallsSearchPage } from './halls/HallsSearchPage';
 import { HallsExplorePage } from './halls/HallsExplorePage';
 
 export {
-  HallsPage,
   ReservationsPage,
   UsersPage,
   MyAccountPage,

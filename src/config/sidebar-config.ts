a268d04e6ba4137f6f2e7b006b1a8dda4056801c
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-03-31 13:11:48 -0600

    WIP: update the sidebar config for the halls option to match the new division

--- src/config/sidebar-config.ts
+++ src/config/sidebar-config.ts
@@ -1,33 +1,54 @@
 import { type IconName } from 'lucide-react/dynamic';
 import { PATHS } from '@/paths';
 
-interface ItemConfig {
+export type ExpandableOptItem = { label: string; path: string };
+
+interface BaseOpt {
   sectionLabel: string;
   iconName: IconName;
   rootPath: string;
 }
 
-const SIDEBAR_ITEMS: ItemConfig[] = [
+interface SimpleOpt extends BaseOpt {
+  type: 'simple';
+}
+
+interface ExpandableOpt extends BaseOpt {
+  type: 'expandable';
+  items: ExpandableOptItem[];
+}
+
+const hallOptItems: ExpandableOptItem[] = [
+  { label: 'Search', path: PATHS.halls.search },
+  { label: 'Explore', path: PATHS.halls.explore }
+];
+
+type ItemConfig = SimpleOpt | ExpandableOpt;
+
+export const SIDEBAR_ITEMS: ItemConfig[] = [
   {
+    type: 'expandable',
     sectionLabel: 'Halls',
     iconName: 'house',
-    rootPath: PATHS.halls
+    rootPath: PATHS.halls.root,
+    items: hallOptItems
   },
   {
+    type: 'simple',
     sectionLabel: 'Reservations',
     iconName: 'calendar',
     rootPath: PATHS.reservations
   },
   {
+    type: 'simple',
     sectionLabel: 'Users',
     iconName: 'users',
     rootPath: PATHS.users
   },
   {
+    type: 'simple',
     sectionLabel: 'My Account',
     iconName: 'user',
     rootPath: PATHS.myAccount
   }
 ];
-
-export { SIDEBAR_ITEMS };

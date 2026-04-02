0c9d5ce9d6168e8ca2198658913cf76ce14cd5e0
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-01 21:43:22 -0600

    feat: create the drawer for the hall creation with a form validated using RHF and zod

--- src/components/ui/CreateHallDrawer.tsx
+++ src/components/ui/CreateHallDrawer.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react';
+import { useState, type ReactNode } from 'react';
 import {
   Drawer,
   DrawerClose,
@@ -14,24 +14,50 @@ import FormField from '../common/FormField';
 import Input from '../common/Input';
 import { Textarea } from './textarea';
 import { Switch } from './switch';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { hallCreateSchema, type HallCreate } from '@/api/schemas/hall.schemas';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import { hallService } from '@/api/services/hall.service';
+import { getErrorMessage } from '@/api/api.utils';
+import { CircleFadingArrowUp } from 'lucide-react';
+import { ICON_SIZE } from '@/constants/ui.constants';
+import SpinnerLoader from '../common/SpinnerLoader';
 
 interface CreateHallDrawerProps {
   children: ReactNode;
+  onSuccess: () => void;
 }
 
-export const CreateHallDrawer = ({ children }: CreateHallDrawerProps) => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-    reset
-  } = useForm<HallCreate>({ resolver: zodResolver(hallCreateSchema) })
+export const CreateHallDrawer = ({ children, onSuccess }: CreateHallDrawerProps) => {
+  const form = useForm<HallCreate>({
+    resolver: zodResolver(hallCreateSchema),
+    defaultValues: {
+      name: '',
+      description: '',
+      is_available: true
+    }
+  });
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const onValidSubmit = async (createData: HallCreate) => {
+    try {
+      await hallService.create(createData);
+
+      toast.success('Hall created successfully');
+
+      onSuccess();
+      setIsOpen(false);
+      form.reset();
+    } catch(e) {
+      const msg = getErrorMessage(e);
+      if (msg) toast.error(msg);
+    }
+  };
 
   return (
-    <Drawer direction='right'>
+    <Drawer open={isOpen} onOpenChange={setIsOpen} direction='right'>
       <DrawerTrigger asChild>
         {children}
       </DrawerTrigger>
@@ -42,48 +68,87 @@ export const CreateHallDrawer = ({ children }: CreateHallDrawerProps) => {
         </DrawerHeader>
         <div className='p-4 h-full flex overflow-y-auto'>
           <form
-          onSubmit={() => {}}
-          className='flex flex-col gap-5 grow'
-          aria-label='Login form'
-          noValidate
-        >
-          <FormField label='name' required>
-            {(id) => (
-              <Input
-                id={id}
-                required
-                iconName='house'
-                placeholder='The outdoor hall'
-                intention={errors.name ? 'danger' : 'brand'}
-                {...register('name')}
-                {...(errors.name ? { 'aria-invalid': true, 'aria-errormessage': `${id}-error` } : {})}
-              />)
-            }
-          </FormField>
+            onSubmit={form.handleSubmit(onValidSubmit)}
+            id='create-hall-form'
+            className='flex flex-col gap-5 grow'
+            aria-label='Form to create a new hall'
+            noValidate
+          >
+            <Controller
+              name='name'
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <FormField label='name' required error={fieldState.error}>
+                  {(id) => (
+                    <Input
+                      {...field}
+                      id={id}
+                      required
+                      iconName='house'
+                      placeholder='The outdoor hall'
+                      intention={fieldState.invalid ? 'danger' : 'brand'}
+                      {...(fieldState.invalid ? { 'aria-invalid': true, 'aria-errormessage': `${id}-error` } : {})}
+                    />)
+                  }
+                </FormField>
+              )}
+            />
 
-          <FormField label='Description' required>
-            {(id) => (
-              <Textarea
-                id={id}
-                placeholder='Type the hall description here.'
-                className='text-sm leading-4.5 h-50 resize-none focus-within:ring-2'
-                {...(errors.description ? { 'aria-invalid': true, 'aria-errormessage': `${id}-error` } : {})}
-              />
-            )}
-          </FormField>
+            <Controller
+              name='description'
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <FormField label='description' required error={fieldState.error}>
+                  {(id) => (
+                    <Textarea
+                      {...field}
+                      id={id}
+                      placeholder='Type the hall description here.'
+                      className='text-sm leading-4.5 h-50 resize-none focus-within:ring-2'
+                      {...(fieldState.invalid ? { 'aria-invalid': true, 'aria-errormessage': `${id}-error` } : {})}
+                    />)
+                  }
+                </FormField>
+              )}
+            />
 
-          <FormField label='Available'>
-            {(id) => (
-              <Switch
-                id={id}
-                {...(errors.is_available ? { 'aria-invalid': true, 'aria-errormessage': `${id}-error` } : {})}
-              />
-            )}
-          </FormField>
-        </form>
+            <Controller
+              name='is_available'
+              control={form.control}
+              render={({ field }) => (
+                <FormField label='available' required>
+                  {(id) => (
+                    <Switch
+                      id={id}
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                    />)
+                  }
+                </FormField>
+              )}
+            />
+          </form>
         </div>
         <DrawerFooter className='flex flex-row'>
-          <Button className='grow' type='submit' disabled={isSubmitting}>Update</Button>
+          <Button
+            className='grow px-2'
+            type='submit'
+            form='create-hall-form'
+            intent='hall'
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting ? (
+              <>
+                <SpinnerLoader size='xs' />
+                <span>Creating</span>
+              </>
+            ) : (
+              <>
+                <span><CircleFadingArrowUp size={ICON_SIZE.MD} /></span>
+                <span>Create</span>
+              </>
+            )}
+          </Button>
           <DrawerClose asChild>
             <Button intent='gray'>Cancel</Button>
           </DrawerClose>

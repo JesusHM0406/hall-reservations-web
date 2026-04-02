1bc28323afae60e6e4de20cc0d00b0e77db7128f
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-02 09:33:22 -0600

    fix: remove extra aria-invalid tailwind class

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -7,7 +7,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
     <textarea
       data-slot="textarea"
       className={cn(
-        "flex field-sizing-content min-h-16 w-full rounded-lg border border-inactive/25 text-xs bg-subtle-white dark:bg-transparent px-2.5 py-2 transition outline-none font-medium focus-visible:ring-2 focus-visible:ring-brand disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-danger/70! aria-invalid:focus-within:ring-danger aria-invalid:ring-destructive/20 md:text-sm dark:disabled:bg-input/80 dark:aria-invalid:border-danger dark:aria-invalid:ring-danger",
+        "flex field-sizing-content min-h-16 w-full rounded-lg border border-inactive/25 text-xs bg-subtle-white dark:bg-transparent px-2.5 py-2 transition outline-none font-medium focus-visible:ring-2 focus-visible:ring-brand disabled:cursor-not-allowed disabled:bg-input/50 disabled:opacity-50 aria-invalid:border-danger/70! aria-invalid:focus-within:ring-danger md:text-sm dark:disabled:bg-input/80 dark:aria-invalid:border-danger dark:aria-invalid:ring-danger",
         className
       )}
       {...props}

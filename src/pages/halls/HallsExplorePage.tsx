08577f075453fed2e3d29c299445f17be8007364
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-02 10:50:14 -0600

    refactor: move the compound/logic and common components into the common folder

--- src/pages/halls/HallsExplorePage.tsx
+++ src/pages/halls/HallsExplorePage.tsx
@@ -2,7 +2,7 @@ import Button from '@/components/ui/Button';
 import { PlusCircle } from 'lucide-react';
 import { ICON_SIZE } from '@/constants/ui.constants';
 import { FilterContainer } from '@/components/ui/FilterContainer';
-import { FilterSelect } from '@/components/ui/FilterSelect';
+import { FilterSelect } from '@/components/common/FilterSelect';
 import { HALL_AVAILABILITY_FILTER_ITEMS } from '@/constants/hall.constants';
 import { useEffect, useState } from 'react';
 import { hallAvailabilityFilterEnum, type HallAvailabilityFilter, type HallPagination } from '@/api/schemas/hall.schemas';

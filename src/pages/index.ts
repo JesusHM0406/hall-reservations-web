1222698f926cd180e27219221e6b6fca740e4dea
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-03 11:34:15 -0600

    refactor: move UsersPage into the new users folder

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@ import { MyAccountPage } from './MyAccountPage';
 import { RegisterPage } from './auth/RegisterPage';
 import { LoginPage } from './auth/LoginPage';
 import { ReservationsPage } from './ReservationsPage';
-import { UsersPage } from './UsersPage';
+import { UsersPage } from './users/UsersPage';
 import { HallDetailPage } from './halls/HallDetailPage';
 import { HallsSearchPage } from './halls/HallsSearchPage';
 import { HallsExplorePage } from './halls/HallsExplorePage';

430d0adfa193bcbd2f341191df3566a454286946
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-06 10:42:04 -0600

    feat: add the functionality to log out the user using the logOut function of the auth context

--- src/pages/myAccount/components/LogoutDialog.tsx
+++ src/pages/myAccount/components/LogoutDialog.tsx
@@ -1,5 +1,6 @@
 import CustomButton from '@/components/ui/Button';
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { useAuth } from '@/hooks/useAuth';
 import type { ReactNode } from 'react';
 
 interface LogoutDialogProps {
@@ -8,6 +9,8 @@ interface LogoutDialogProps {
 
 export const LogoutDialog = ({ children }: LogoutDialogProps) => {
   const cancelBtnId = 'cancel-logout-button';
+
+  const { logOut } = useAuth();
 
   return (
     <Dialog>
@@ -27,7 +30,10 @@ export const LogoutDialog = ({ children }: LogoutDialogProps) => {
           <DialogDescription>Are you sure you want to log out?</DialogDescription>
         </DialogHeader>
         <DialogFooter className='flex-row justify-end'>
-          <CustomButton intent='danger' >
+          <CustomButton
+            intent='danger'
+            onClick={logOut}
+          >
             Log Out
           </CustomButton>
           <DialogClose asChild>

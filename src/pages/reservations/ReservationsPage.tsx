633eb57ac49850cdaf9db276cb871b98e94daa91
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-07 09:40:59 -0600

    feat: handle search params in the pagination

--- src/pages/reservations/ReservationsPage.tsx
+++ src/pages/reservations/ReservationsPage.tsx
@@ -3,8 +3,64 @@ import { Pagination } from '@/components/common/Pagination';
 import { FilterContainer } from '@/components/ui/FilterContainer';
 import { RES_STATUS_FILTER_ITEMS } from '@/constants/reservations.constants';
 import { ReservationCard } from './components/ReservationCard';
+import { useSearchParams } from 'react-router';
+import {
+  reservationStatusFilterEnum,
+  type ReservationPagination,
+  type ReservationStatusFilter
+} from '@/api/schemas/reservation.schemas';
+import { useEffect, useState } from 'react';
+import { reservationService } from '@/api/services/reservation.service';
+import { getErrorMessage } from '@/api/api.utils';
+import { toast } from 'sonner';
 
 export const ReservationsPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const rawPage = parseInt(searchParams.get('page') || '1', 10);
+  const page = isNaN(rawPage) || rawPage < 1 ? 1 : rawPage;
+
+  const rawStatus = searchParams.get('status');
+  const statusParse = reservationStatusFilterEnum.safeParse(rawStatus);
+  const status: ReservationStatusFilter = statusParse.success ? statusParse.data : 'all';
+
+  const [usersPag, setUsersPag] = useState<ReservationPagination | null>(null);
+
+  useEffect(() => {
+    let isCurrent = true;
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      try {
+        const data = await reservationService.all(
+          {page, ...(status === 'all' ? {} : { status })},
+          controller
+        );
+
+        if (isCurrent) {
+          setUsersPag(data);
+          setSearchParams(
+            { page: page.toString(), ...(status ? { status: status } : {}) }
+          );
+        }
+      } catch(e) {
+        const msg = getErrorMessage(e);
+        if (isCurrent && msg) toast.error(msg);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      isCurrent = false;
+      controller.abort();
+    };
+  }, [page, status, setSearchParams]);
+
+  const handleStatusFilterClick = (value: ReservationStatusFilter) => {
+    setSearchParams({ page: page.toString(), status: value});
+  };
+
   return (
     <div className='max-w-xl w-full mx-auto flex flex-col gap-7 grow'>
       <section className='flex justify-between gap-3 items-center'>
@@ -20,9 +76,9 @@ export const ReservationsPage = () => {
             {(id) => (
               <CustomSelect
                 id={id}
-                value='confirmed'
+                value={status}
                 items={RES_STATUS_FILTER_ITEMS}
-                onValueChange={() => {}}
+                onValueChange={handleStatusFilterClick}
                 placeholder='Select status'
               />
             )}

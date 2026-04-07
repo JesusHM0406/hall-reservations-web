2dbc9ea990ee263216de77bb1e3ea3a7d9ff3ae4
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-07 07:31:56 -0600

    refactor: move ReservationsPage to reservations folder

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
 import { MyAccountPage } from './myAccount/MyAccountPage';
 import { RegisterPage } from './auth/RegisterPage';
 import { LoginPage } from './auth/LoginPage';
-import { ReservationsPage } from './ReservationsPage';
+import { ReservationsPage } from './reservations/ReservationsPage';
 import { UsersPage } from './users/UsersPage';
 import { HallDetailPage } from './halls/HallDetailPage';
 import { HallsSearchPage } from './halls/HallsSearchPage';

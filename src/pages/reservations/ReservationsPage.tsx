de00da0f56fc653cf648212f8acc81fc78a88d49
JesusHM0406 <jesus.h.mora0406@gmail.com>
2026-04-08 07:41:31 -0600

    feat: implement user name and hall name filter

--- src/pages/reservations/ReservationsPage.tsx
+++ src/pages/reservations/ReservationsPage.tsx
@@ -13,18 +13,34 @@ import { useEffect, useState } from 'react';
 import { reservationService } from '@/api/services/reservation.service';
 import { getErrorMessage } from '@/api/api.utils';
 import { toast } from 'sonner';
+import { SearchInput } from '@/components/common/SearchInput';
+import { useDebounce } from '@/hooks/useDebounce';
 
 export const ReservationsPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const rawPage = parseInt(searchParams.get('page') || '1', 10);
+  const rawPage = parseInt(searchParams.get('page') ?? '1', 10);
   const page = isNaN(rawPage) || rawPage < 1 ? 1 : rawPage;
 
   const rawStatus = searchParams.get('status');
   const statusParse = reservationStatusFilterEnum.safeParse(rawStatus);
   const status: ReservationStatusFilter = statusParse.success ? statusParse.data : 'all';
 
+  const username = searchParams.get('user_name') ?? '';
+  const hallname = searchParams.get('hall_name') ?? '';
+
   const [resPag, setResPag] = useState<ReservationPagination | null>(null);
+
+  const [usernameVal, setUsernameVal] = useState<string>(username);
+  const [hallnameVal, setHallnameVal] = useState <string>(hallname);
+
+  const debouncedUser = useDebounce(usernameVal, 400);
+  const debouncedHall = useDebounce(hallnameVal, 400);
+
+  const getNameSearch = () => ({
+    ...(debouncedUser.length > 2 ? { user_name: debouncedUser } : {}),
+    ...(debouncedHall.length > 2 ? { hall_name: debouncedHall } : {})
+  });
 
   useEffect(() => {
     let isCurrent = true;
@@ -33,14 +49,22 @@ export const ReservationsPage = () => {
     const fetchData = async () => {
       try {
         const data = await reservationService.all(
-          {page, ...(status === 'all' ? {} : { status })},
+          {
+            page,
+            ...(status === 'all' ? {} : { status }),
+            ...(getNameSearch())
+          },
           controller
         );
 
         if (isCurrent) {
           setResPag(data);
           setSearchParams(
-            { page: page.toString(), ...(status ? { status: status } : {}) }
+            {
+              page: page.toString(),
+              ...(status ? { status: status } : {}),
+              ...(getNameSearch())
+            }
           );
         }
       } catch(e) {
@@ -55,14 +79,23 @@ export const ReservationsPage = () => {
       isCurrent = false;
       controller.abort();
     };
-  }, [page, status, setSearchParams]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page, status, debouncedUser, debouncedHall]);
 
   const handleStatusFilterClick = (value: ReservationStatusFilter) => {
-    setSearchParams({ page: page.toString(), status: value});
+    setSearchParams({
+      page: page.toString(),
+      status: value,
+      ...(getNameSearch())
+    });
   };
 
   const handlePageClick = (num: number) => {
-    setSearchParams({ page: num.toString(), status});
+    setSearchParams({
+      page: num.toString(),
+      status,
+      ...(getNameSearch())
+    });
   };
 
   return (
@@ -75,18 +108,47 @@ export const ReservationsPage = () => {
 
       <section>
         <h2 className='font-bold uppercase text-sm text-gray tracking-wider'>Filters</h2>
-        <div>
-          <FilterContainer label='Status'>
-            {(id) => (
-              <CustomSelect
-                id={id}
-                value={status}
-                items={RES_STATUS_FILTER_ITEMS}
-                onValueChange={handleStatusFilterClick}
-                placeholder='Select status'
-              />
-            )}
-          </FilterContainer>
+        <div className='flex flex-col gap-4'>
+          <div>
+            <FilterContainer label='Status'>
+              {(id) => (
+                <CustomSelect
+                  id={id}
+                  value={status}
+                  items={RES_STATUS_FILTER_ITEMS}
+                  onValueChange={handleStatusFilterClick}
+                  placeholder='Select status'
+                />
+              )}
+            </FilterContainer>
+          </div>
+          <div className='flex flex-col gap-4'>
+            <FilterContainer label='User name'>
+              {(id) => (
+                <SearchInput
+                  id={id}
+                  value={usernameVal}
+                  setSearch={setUsernameVal}
+                  placeholder='John Doe'
+                  ariaLabel='Filter reservations by user name'
+                  clearLabel='Clear user name filter'
+                />
+              )}
+            </FilterContainer>
+
+            <FilterContainer label='Hall name'>
+              {(id) => (
+                <SearchInput
+                  id={id}
+                  value={hallnameVal}
+                  setSearch={setHallnameVal}
+                  placeholder='Cascade Falls Event Center'
+                  ariaLabel='Filter reservations by hall name'
+                  clearLabel='Clear hall name filter'
+                />
+              )}
+            </FilterContainer>
+          </div>
         </div>
       </section>
 
